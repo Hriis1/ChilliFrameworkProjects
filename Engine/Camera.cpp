92068371ca7a31de8a104b23a476e02cdae404ed
hrishd <hrishd@abv.bg>
2023-06-17 01:29:50 +0300

    Added some comments for the camera scaling

--- Engine/Camera.cpp
+++ Engine/Camera.cpp
@@ -19,7 +19,9 @@ void Camera::drawPolyLine(std::vector<Vec2<float>> poly, Color c)
 {
 	for (auto& vert : poly)
 	{
+		//Translate first so the position is based on the camera
 		vert -= _pos;
+		//scale after translating so it scales around the camera position
 		vert *= _scale;
 	}
 	_ct.drawPolyLine(std::move(poly), c);
