ae7f67c3d38e77f2b75dd0ba2c2c18cec3884118
hrishd <hrishd@abv.bg>
2023-06-21 00:31:50 +0300

    Engine - fixed some mistakes in the math file

--- Engine/MathKEK.h
+++ Engine/MathKEK.h
@@ -13,8 +13,8 @@ template<typename T>
 T pointLineDistance(const Vec2<T>& linePoint0, const Vec2<float>& linePoint1, const Vec2<T>& q)
 {
 	const T a = linePoint0._y - linePoint1._y;
-	const T a = linePoint1._x - linePoint0._x;
-	const T c = linePoint0._x * linePoint1._y - p1._x * p0._y;
+	const T b = linePoint1._x - linePoint0._x;
+	const T c = linePoint0._x * linePoint1._y - linePoint1._x * linePoint0._y;
 
 	return std::abs(a * q._x + b * q._y + c) / std::sqrt(sq(a) + sq(b));
 }
