b597b27e98378828deec52fa40a1a2902360e8c1
hrishd <hrishd@abv.bg>
2023-07-16 15:32:23 +0300

    SimplePlatformer - Disabled up and down movement of player

--- SimplePlatformer/Player.cpp
+++ SimplePlatformer/Player.cpp
@@ -8,7 +8,7 @@ void Player::update(float deltaTime)
 
 void Player::updateMovement(float deltaTime)
 {
-	if (_wnd.kbd.KeyIsPressed('S'))
+	/*if (_wnd.kbd.KeyIsPressed('S'))
 	{
 		Vec2<float> moveAmount = Vec2<float>(0, -PLAYER_SPEED * deltaTime);
 		Vec2<float> newPos = getPos() + moveAmount;
@@ -21,19 +21,23 @@ void Player::updateMovement(float deltaTime)
 		Vec2<float> newPos = getPos() + moveAmount;
 		setPos(newPos);
 		_rect.moveBy(moveAmount);
-	}
-	if (_wnd.kbd.KeyIsPressed('A'))
+	}*/
+
+	if (_wnd.kbd.KeyIsPressed('A') || _wnd.kbd.KeyIsPressed('D'))
 	{
-		Vec2<float> moveAmount = Vec2<float>(-PLAYER_SPEED * deltaTime, 0);
-		Vec2<float> newPos = getPos() + moveAmount;
-		setPos(newPos);
-		_rect.moveBy(moveAmount);
-	}
-	if (_wnd.kbd.KeyIsPressed('D'))
-	{
-		Vec2<float> moveAmount = Vec2<float>(PLAYER_SPEED * deltaTime, 0);
-		Vec2<float> newPos = getPos() + moveAmount;
-		setPos(newPos);
-		_rect.moveBy(moveAmount);
+		if (_wnd.kbd.KeyIsPressed('A'))
+		{
+			Vec2<float> moveAmount = Vec2<float>(-PLAYER_SPEED * deltaTime, 0);
+			Vec2<float> newPos = getPos() + moveAmount;
+			setPos(newPos);
+			_rect.moveBy(moveAmount);
+		}
+		if (_wnd.kbd.KeyIsPressed('D'))
+		{
+			Vec2<float> moveAmount = Vec2<float>(PLAYER_SPEED * deltaTime, 0);
+			Vec2<float> newPos = getPos() + moveAmount;
+			setPos(newPos);
+			_rect.moveBy(moveAmount);
+		}
 	}
 }
